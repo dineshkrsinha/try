bbf1a4ba15133d589b1697006bde9a7cff0ef44c
Dinesh Sinha <dinesh@vidyo.com>
2018-04-21 17:55:54 -0400

    updated pull.sh

--- conFusion/pull.sh
+++ conFusion/pull.sh
@@ -1,32 +1,19 @@
 
 #!/bin/bash
 echo "hello, $USER. I wish to list some files of yours"
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/shared/leader.ts
-mv /Users/dinesh/Downloads/leader.ts /Users/dinesh/bootstrap/angular/conFusion/src/app/shared/leader.ts
-
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/services/leader.service.ts
-mv /Users/dinesh/Downloads/leader.service.ts /Users/dinesh/bootstrap/angular/conFusion/src/app/services/leader.service.ts
 
 
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/about/about.component.ts
-mv /Users/dinesh/Downloads/about.component.ts /Users/dinesh/bootstrap/angular/conFusion/src/app/about/about.component.ts
+rm /Users/dinesh/bootstrap/try/conFusion/src/app/services/promotion.service.ts
+mv /Users/dinesh/Downloads/promotion.service.ts /Users/dinesh/bootstrap/try/conFusion/src/app/services/promotion.service.ts
 
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/about/about.component.html
-mv /Users/dinesh/Downloads/about.component.html /Users/dinesh/bootstrap/angular/conFusion/src/app/about/about.component.html
+rm /Users/dinesh/bootstrap/try/conFusion/src/app/services/leader.service.ts
+mv /Users/dinesh/Downloads/leader.service.ts /Users/dinesh/bootstrap/try/conFusion/src/app/services/leader.service.ts
 
+rm /Users/dinesh/bootstrap/try/conFusion/src/app/services/feedback.service.ts
+mv /Users/dinesh/Downloads/feedback.service.ts /Users/dinesh/bootstrap/try/conFusion/src/app/services/feedback.service.ts
 
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/home/home.component.ts
-mv /Users/dinesh/Downloads/home.component.ts /Users/dinesh/bootstrap/angular/conFusion/src/app/home/home.component.ts
+rm /Users/dinesh/bootstrap/try/conFusion/src/app/contact/contact.component.ts
+mv /Users/dinesh/Downloads/contact.component.ts /Users/dinesh/bootstrap/try/conFusion/src/app/contact/contact.component.ts
 
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/home/home.component.html
-mv /Users/dinesh/Downloads/home.component.html /Users/dinesh/bootstrap/angular/conFusion/src/app/home/home.component.html
-
-
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/app.module.ts
-mv /Users/dinesh/Downloads/app.module.ts /Users/dinesh/bootstrap/angular/conFusion/src/app/app.module.ts
-
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/header/header.component.html
-mv /Users/dinesh/Downloads/header.component.html /Users/dinesh/bootstrap/angular/conFusion/src/app/header/header.component.html
-
-rm /Users/dinesh/bootstrap/angular/conFusion/src/app/app-routing/routes.ts
-mv /Users/dinesh/Downloads/routes.ts /Users/dinesh/bootstrap/angular/conFusion/src/app/app-routing/routes.ts
+rm /Users/dinesh/bootstrap/try/conFusion/src/app/contact/contact.component.html
+mv /Users/dinesh/Downloads/contact.component.html /Users/dinesh/bootstrap/try/conFusion/src/app/contact/contact.component.html
